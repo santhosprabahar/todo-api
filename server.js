c8031cc10473cf6172959741250f158894f67514
santhos <santhosprabahar@gmail.com>
2016-06-20 02:04:20 +0530

    error change

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var portno = process.env.PORT || 3000;
 
-app.get('/' , function (res, req) {
+app.get('/' , function (req, res) {
 	res.send("todo api");
+});
+app.listen(portno, function()
+{
+	console.log("server up and running at" + portno);
 });
 
