75f63257e4d251bec0dc0da466b1a3d8b042e85b
santhos <santhosprabahar@gmail.com>
2016-06-20 15:36:48 +0530

    delete added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var _ = require("underscore");
 var app = express();
 var portno = process.env.PORT || 3000;
 var todonextid = 1;
@@ -13,13 +14,20 @@ app.get('/' , function (req, res) {
 });
 
 app.get('/todos', function (req, res){
-	res.json(todos);
+	res.send(todos);
 });
 
 
 app.post('/todos', function (req, res)
-{
-	var body = req.body;
+{	
+	var body = _.pick(req.body,"description", "completed");
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0)
+	{
+		return res.status(404).send("404 error");
+	}
+
+
+	body.description= body.description.trim();
 	// console.log("description" + body.description);
 	body.id = todonextid++;
 	 todos.push(body);
@@ -27,26 +35,45 @@ app.post('/todos', function (req, res)
 });
 
 
-app.get('/todos/:id',function (req, res){
-	var todoid = parseInt(req.params.id);
+app.delete('/todos/:id', function (req, res){
+	var todoid = parseInt(req.params.id, 10);
 	var matchedtodo;
-	todos.forEach(function (todo)
-	{
+	matchedtodo = _.findWhere(todos, {id:todoid});
 
-		if(todoid === todo.id)
+	if(!matchedtodo)
+	{
+		res.status(404).json({"error": "id not found"});
+	}else
+	{
+		todos = _.without(todos, matchedtodo)
+		res.send(matchedtodo);
+	}
 	
-		matchedtodo=todo;
-	});
+});
 
-	if (matchedtodo)
-	{
-		res.json(matchedtodo)
-	}
-	else
-	{
-		res.status(404).send("404 error");
+app.get('/todos/:id',function (req, res){
+	var todoid = parseInt(req.params.id, 10);
+	var matchedtodo;
+	matchedtodo= _.findWhere(todos,{id:todoid})
 
-	}
+	 // var matchedtodo;
+	 // todos.forEach(function (todo)
+	 // {
+
+	 // 	if(todoid === todo.id)
+	
+	 // 	matchedtodo=todo;
+	 // });
+
+	 if (matchedtodo)
+	 {
+	 	res.json(matchedtodo)
+	 }
+	 else
+	 {
+	 	res.status(404).send("404 error");
+
+	 }
 });
 app.listen(portno, function()
 {
